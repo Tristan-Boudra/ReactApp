71488ddf5dbeccb70aa1310469598ab4d3bcfc56
Tristan-Boudra <boudratristan@gmail.com>
2023-07-18 19:22:59 +0200

    props

--- project-01/src/App.js
+++ project-01/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import DisplayExpense from './components/DisplayExpense';
 
 function App() {
   const expenses = [
@@ -20,33 +20,14 @@ function App() {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2021, 5, 12),
+      date: new Date(2021, 5, 14),
     },
   ];
 
   return (
-    <div>
+    <div className="expenses">
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <DisplayExpense item={expenses} />
     </div>
   );
 }
